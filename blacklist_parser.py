3b10b50dc8521e5aaa183cbe991f17dd2e8ddee1
jonny166 <kathy.church@gmail.com>
2015-09-23 21:01:50 -0400

    Add more error handling and break up large functions

--- blacklist_parser.py
+++ blacklist_parser.py
@@ -48,46 +48,57 @@ def get_blacklist(source):
 #end get_blacklist
 
 
-def generate_report(csv_data, report):
-    '''Convert CSV data to report'''
-    logger.info("generate_report")
-    
-    field_list = "# DstIP,DstPort"
-    header_end = csv_data.find(field_list)
-    if not header_end:
-        logger.error("CSV File Header does not conform to expected formatting")
-        return
+def parse_header(header):
+    '''get the report name and time from the header'''
+    if len(header) < 3:
+        logger.error("Blacklist header too short")
+        return None, None
 
-    # Parse the file header
-    header = csv_data[:header_end].splitlines()
     report_name = header[1].strip('#').strip(' ')
     report_timestamp = header[2].strip('#').strip(' ')
-    report_datetime = datetime.datetime.strptime(report_timestamp, 
-                                                 "Last updated: %Y-%m-%d %H:%M:%S (%Z)")
+    report_datetime = None
+    try:
+        report_datetime = datetime.datetime.strptime(report_timestamp, 
+                                                     "Last updated: %Y-%m-%d %H:%M:%S (%Z)")
+    except ValueError, e:
+        logger.error("Timestamp in blacklist not formatted as expected: %s", report_timestamp)
+        logger.debug(traceback.format_exc())
 
-    blacklist_dict = defaultdict(list)
-    # get all the rows of data that appear after the header block
-    rows = csv_data[header_end+len(field_list):].splitlines()
+    return report_name, report_datetime
+#end parse_header
 
-    # Remove trailer from data list and parse it
-    trailer = rows.pop()
+
+def parse_trailer(trailer):
+    '''get the data count from the trailer'''
     parsed_trailer = re.match(r"# Number of entries: ([0-9]+)", trailer)
-    num_entries = int(parsed_trailer.group(1))
+    if parsed_trailer:
+        num_entries = int(parsed_trailer.group(1))
+    else:
+        logger.error("Trailer in blacklist not formatted as expected")
+        num_entries = -1
+
+    return num_entries
+#end parse_trailer
+
+
+def generate_report(report, report_name, num_entries, report_datetime, blacklist_dict):
+    """Write the data to the report
+    
+    Args:
+        report: Stream to write the report to. Usually an open file.
+        report_name: Name of the blacklist report
+        num_entires: Integer representing number of entries in the blacklist
+        report_datetime: datetime object from timestamp in the blacklist
+        blacklist_dict: dict with the blacklist data.
+           example: {"KINS C&C": [(89.65.63.95,443),
+                                  (91.214.209.193,443)],
+                     "Dridex C&C": [(91.201.155.96,443)]}
+        """
 
     report.write("%s Blacklist Report\n" % report_name)
     report.write("(%d IP's found at %s):\n" % (num_entries, 
                                              report_datetime.strftime("%b %d %H:%M:%S %Y")))
-
-    # Walk the list of rows
-    reader = csv.reader(rows)
-    for row in reader:
-        if len(row) != 3:
-            logger.error("Skipping invalid data row: %s", str(row))
-            continue
-
-        destIP, destPort, description = row
-        blacklist_dict[description].append((destIP, destPort))
-
+        
     # Write out each section in alphabetical order
     for description in sorted(blacklist_dict.keys()):
         entries = blacklist_dict[description]
@@ -99,8 +110,55 @@ def generate_report(csv_data, report):
 
 
 
+def parse_blacklist(data, report):
+    """Extract data from blacklist and create a report
+
+    Parses the necessary data from the input and writes out a report
+
+    Args:
+        data: String containing the retrieved blacklist report
+        report: Stream to write the report to. Usually an open file.
+        """
+
+    logger.info("parse_blacklist")
+    
+    field_list = "# DstIP,DstPort\n"
+    header_end = data.find(field_list)
+    if not header_end:
+        logger.error("CSV File Header does not conform to expected formatting")
+        return
+
+    # Parse the file header
+    report_name, report_datetime = parse_header(data[:header_end].splitlines())
+
+    blacklist_dict = defaultdict(list)
+    # get all the rows of data that appear after the header block
+    rows = data[header_end+len(field_list):].splitlines()
+
+    # Remove trailer from data list and parse it
+    num_entries = parse_trailer(rows.pop())
+
+    # Walk the list of rows
+    try:
+        reader = csv.reader(rows)
+        for row in reader:
+            if len(row) != 3:
+                logger.error("Skipping invalid data row: %s", str(row))
+                continue
+
+            dest_IP, dest_port, description = row
+            blacklist_dict[description].append((dest_IP, dest_port))
+    except csv.Error, e:
+        logger.error("CSV parsing failed for blacklist")
+        logger.debug(traceback.format_exc())
+
+    generate_report(report, report_name, num_entries, report_datetime, blacklist_dict)
+#end parse_blacklist
+
+
+
 def main():
-    '''Retrieve CSV blacklist and generate report'''
+    """Retrieve CSV blacklist and generates report"""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbose", help="output info and debug messages",
@@ -122,17 +180,22 @@ def main():
     if args.stdout:
         report = sys.stdout
     else:
-        report = open(args.dest, 'w')
+        try:
+            report = open(args.dest, 'w')
+        except IOError, e:
+            logger.error("Failed to open report output file")
+            logger.debug(traceback.format_exc())
 
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging_level)
     logger.info('Starting blacklist parser')
     try:
         data = get_blacklist(args.source)
-        generate_report(data, report)
-    except Exception, e:
+        parse_blacklist(data, report)
+    except Exception, e: #catch-all in case something falls through
         logger.error(traceback.format_exc())
-        
-    report.close()
+    finally:
+        report.close()
+
     logger.info('Finished blacklist parser')
 #end main
 
